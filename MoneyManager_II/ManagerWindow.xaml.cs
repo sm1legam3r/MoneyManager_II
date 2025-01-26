9165bc7704d322ee5171bfbefc96923eb25acb45
shumkov <dfre.griffin111@gmail.com>
2025-01-26 12:28:59 +0300

    Добавьте файлы проекта.

---
+++ MoneyManager_II/ManagerWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyManager_II
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        public ManagerWindow()
+        {
+            InitializeComponent();
+            ManagerFrame.Navigate(new HomePage());
+        }
+
+        private void HomeImageClick(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Navigate(new HomePage());
+        }
+
+        private void IncomeImageClick(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Navigate(new IncomePage());
+        }
+
+        private void SpendingsImageClick(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Navigate(new SpendingsPage());
+        }
+
+        private void AccountImageClick(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Navigate(new AccountPage());
+        }
+
+        private void SettingsImageClick(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Navigate(new SettingsPage());
+        }
+    }
+}
---
+++ MoneyManager_II/RegistrationPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoneyManager_II
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        private bool registrationSuccess = false;
+
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Registrate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            registrationSuccess = CheckDataRegistration();
+            if (registrationSuccess)
+            {
+                MessageBox.Show("Вы успешно зарегистрировались!😀");
+                NavigationService.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("Регистраиця провалена!😢");
+            }
+        }
+
+        private bool CheckDataRegistration()
+        {
+            bool emptyFields = SurnameTextBox.Text.Equals("") || NameTextBox.Text.Equals("") ||
+                               LoginTextBox.Text.Equals("") || PasswordBox.Password.Equals("");
+            return !emptyFields;
+        }
+
+        private void GoBackTextClick(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
