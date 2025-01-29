3db2a00a2494d670185b1ece9309af259172cea2
sm1legam3r <smayl.igrok256@gmail.com>
2025-01-29 10:08:43 +0300

    added LiveCharts

---
+++ MoneyManager_II/Model/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager_II.Model
+{
+    public class Database
+    {
+        private SqlConnection sqlConnection = new SqlConnection(
+            "@Data Source=510EC12\\MMSQLSERVER; Initial Catalog=MoneyManagerDB; Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
