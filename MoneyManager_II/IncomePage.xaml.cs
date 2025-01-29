386faa853025dd4c8e2e9d49969421a6d5e9cbec
sm1legam3r <smayl.igrok256@gmail.com>
2025-01-29 09:54:59 +0300

    #1

--- MoneyManager_II/IncomePage.xaml.cs
+++ MoneyManager_II/IncomePage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts.Wpf;
+using LiveCharts;
 
 namespace MoneyManager_II
 {
@@ -23,6 +25,33 @@ namespace MoneyManager_II
         public IncomePage()
         {
             InitializeComponent();
+
+            pieChart.Series = new SeriesCollection()
+            {
+                new PieSeries
+                {
+                    Values = new ChartValues<double> {20},
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Colors.Red),
+                    LabelPoint = chartPoint => $"{chartPoint.Y} ({chartPoint.Participation:P})"
+                },
+                new PieSeries
+                {
+                    Values = new ChartValues<double> {40},
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Colors.Green),
+                    LabelPoint = chartPoint => $"{chartPoint.Y} ({chartPoint.Participation:P})"
+                },
+                new PieSeries
+                {
+                    Values = new ChartValues<double> {30},
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Colors.Blue),
+                    LabelPoint = chartPoint => $"{chartPoint.Y} ({chartPoint.Participation:P})"
+                }
+            };
+
+            pieChart.LegendLocation = LegendLocation.Right;
         }
     }
 }
