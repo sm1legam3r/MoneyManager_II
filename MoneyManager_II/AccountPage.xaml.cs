cfd7a2d0c6faf8664a95242a7140d850b4b362df
sm1legam3r <smayl.igrok256@gmail.com>
2025-01-30 10:07:24 +0300

    AccountPage upgrade #1

--- MoneyManager_II/AccountPage.xaml.cs
+++ MoneyManager_II/AccountPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyManager_II.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace MoneyManager_II
     /// </summary>
     public partial class AccountPage : Page
     {
-        public AccountPage()
+        public AccountPage(User user)
         {
             InitializeComponent();
+            NameTextBlock.Text = $"{user.getName()} {user.getSurname()}";
+            IdTextBlock.Text = $"ID: {user.getId()}";
         }
     }
 }
